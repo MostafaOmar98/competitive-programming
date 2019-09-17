967b1ea7db2c7e8b51da1a9f0f8a7bf6c62744ce
MostafaOmar98 <mostafa.omar.m.s@gmail.com>
2019-09-17 03:15:28 +0200

    Add files via upload

--- Bellman.cpp
+++ Bellman.cpp
@@ -3,44 +3,7 @@ vector<pair<int, int> > adj[MAX_N];
 int dist[MAX_N];
 int n, m;
 
-bool bellman(int src)
-{
-    clr(dist, 0x7F);
-    dist[src] = 0;
-    deque<int> dq;
-    dq.push_back(src);
-    for (int i = 0; i < n; ++i)
-    {
-        for (int j = 0, size = sz(dq); j < size; ++j)
-        {
-            int u = dq.front();
-            dq.pop_front();
-            for (auto e : adj[u])
-            {
-                int v = e.first, c = e.second;
-                if (dist[v] > dist[u] + c)
-                {
-                    dq.push_back(v);
-                    dist[v] = dist[u] + c;
-                }
-            }
-        }
-        if (!sz(dq))
-            break;
-        if (i == n - 1 && sz(dq))
-            return 1;
-    }
-    return 0;
-}
-
-
-
-
-int dist[202];
-bool inQ[202];
-int n;
-
-void bellman(int src)
+int bellman(int src)
 {
     clr(dist, 0x7F);
     clr(inQ, 0);
@@ -56,11 +19,22 @@ void bellman(int src)
         while(size--)
         {
             int u = q.front();
-            inQ[u] = 0;
             q.pop();
-            for (auto v : adj[u])
+            for (int v = 0; v < n; ++v)
             {
-                // relax;
+                for (auto e : adj[u])
+                {
+                    int v = e.first, c = e.second;
+                    if (dist[v] > dist[u] + c)
+                    {
+                        dist[v] = dist[u] + c;
+                        if (!inQ[v])
+                        {
+                            q.push(v);
+                            inQ[v] = 1;
+                        }
+                    }
+                }
             }
         }
         if (!sz(q))
