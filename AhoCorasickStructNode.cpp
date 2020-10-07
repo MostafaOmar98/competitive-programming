97d45db09e76ea621d5d53d35289eb6546650c83
MostafaOmar98 <mostafa.omar.m.s@gmail.com>
2020-10-07 06:35:18 +0200

    Add files via upload

--- AhoCorasickStructNode.cpp
+++ AhoCorasickStructNode.cpp
@@ -1,76 +1,142 @@
+// update MAX_NODES and alphabet size
+const int K = 63;
+
+// update Alphabet indexing
+inline int getIndex(char c)
+{
+    if (isalpha(c))
+    {
+        if (islower(c))
+            c -= 'a';
+        else
+            c = c - 'A' + 26;
+    }
+    else if (c >= '0' && c <= '9')
+        c = c - '0' + 52;
+    else
+        c = 62;
+    return c;
+}
+
 struct AhoCorasick{
     struct Node{
-        int id, pId, msk, suffixLink;
-        vector<int> nxt, t; // Doesn't work with arrays?
-        char c; // [0, 26];
-        Node(int id, int pId, int msk, char c) : id(id), pId(pId), msk(msk), c(c)
+        int link, go[K];
+
+        // update collective
+        int countVis;
+        Node()
         {
-//            memset(nxt, -1, sizeof(nxt));
-//            memset(t, -1, sizeof(t));
-            nxt.resize(26, -1);
-            t.resize(26, -1);
-            suffixLink = -1;
+            memset(go, 0, sizeof(go));
+            link = 0;
+            // update collective
+            countVis = 0;
         }
     };
-    AhoCorasick(const vector<string> &strs)
+
+    vector<Node> t;
+    int addNode()
     {
-        addNode(0, 0);
-        int i = 0;
-        for (const string &s : strs)
-            insert(s, i++);
-        queue<int> q;
-        q.push(0);
-        while(!q.empty())
-        {
-            int u = q.front();
-            q.pop();
-            get_link(u);
-            for (int c = 0; c < 26; ++c)
-                if (v[u].nxt[c] != -1)
-                    q.push(v[u].nxt[c]);
-        }
-    }
-    vector<Node> v;
-    int addNode(char c, int pId)
-    {
-        v.push_back(Node(sz(v), pId, 0, c));
-        return (int)(v.size()) - 1;
+        t.push_back(Node());
+        return sz(t) - 1;
     }
 
-    void insert(const string &s, int i)
+    Node& operator[](int i){
+        return t[i];
+    }
+
+    vector<int> stringIndex;
+    void init(const vector<string> &strs)
+    {
+        // Might want to reserve with MAX_NODES in constructor
+        t.clear();
+        addNode();
+
+        stringIndex.clear();
+        stringIndex.resize(sz(strs));
+        for (int i = 0; i < strs.size(); ++i)
+            stringIndex[i] = insert(strs[i]); // update : put colletive of one node information
+
+        pull_info();
+    }
+
+    int insert(const string &s) // update : put collective of one node information
     {
         int cur = 0;
         for (char c : s)
         {
-            c -= 'a';
-            if (v[cur].nxt[c] == -1)
-                v[cur].nxt[c] = addNode(c, cur);
-            cur = v[cur].nxt[c];
+            c = getIndex(c);
+            if (t[cur].go[c] == 0)
+            {
+                int x = addNode();
+                t[cur].go[c] = x;
+            }
+            cur = t[cur].go[c];
         }
-        v[cur].msk |= (1<<i);
+        // Careful if duplicate strings are allowed. Make sure to add to collective, not just initialize
+        // update : collective
+
+        return cur;
     }
 
-    int get_link(int cur)
+    // push_info
+//     vector<int> v;
+
+    void pull_info()
     {
-        if (!cur || !v[cur].pId)
-            return 0;
-        int &ret = v[cur].suffixLink;
-        if (~ret)
-            return ret;
-        ret = match(get_link(v[cur].pId), v[cur].c);
-        v[cur].msk |= v[ret].msk;
-        return ret;
+        queue<int> q;
+        t[0].link = 0;
+        // push_info
+//         v.push_back(0);
+
+        for (char c = 0; c < K; ++c)
+        {
+            int &nxt = t[0].go[c];
+            if (nxt != 0)
+            {
+                q.push(nxt);
+
+                // push_info
+//                 v.push_back(nxt);
+            }
+        }
+
+        while(!q.empty())
+        {
+            int cur = q.front();
+            q.pop();
+
+            int curLink = t[cur].link;
+            // update collective t[cur].collective += t[curLink].collective
+
+
+            for (char c = 0; c < K; ++c)
+            {
+                int &nxt = t[cur].go[c];
+                if (nxt == 0)
+                    nxt = t[curLink].go[c];
+                else
+                {
+                    q.push(nxt);
+                    t[nxt].link = t[curLink].go[c];
+
+                    // push_info
+//                     v.push_back(nxt);
+                }
+            }
+        }
+
+        // push_info
+//        reverse(v.begin(), v.end());
     }
-    // char c is [0, 26]
-    int match(int cur, char c)
-    {
-        if (v[cur].nxt[c] != -1)
-            return v[cur].nxt[c];
-        if (!cur)
-            return 0;
-        int &ret = v[cur].t[c];
-        if (~ret)
-            return ret;
-        return match(get_link(cur), c);
-    }
-};
+
+    // pushes info to nodes in the sequence of suffix links
+//    void push_info()
+//    {
+//        for (int cur : v)
+//        {
+//            int link = t[cur].link;
+//            // update push collective info from cur to get_link(cur). Example: t[link].freq += t[cur].freq;
+//            t[link].countVis += t[cur].countVis;
+//        }
+//    }
+}dict;
