b697607e6c3cba29ed37afa9806da5821858b981
MostafaOmar98 <mostafa.omar.m.s@gmail.com>
2020-11-28 14:38:13 +0200

    Added with bitsets and getting contributing vector indices

--- Algorithms/math/XOR-trick.cpp
+++ Algorithms/math/XOR-trick.cpp
@@ -31,3 +31,61 @@ struct VectorSpace{
         return false;
     }
 };
+
+
+// vector space with bitsets and getting indices of contributing vectors
+// index[i] is a bitset with 1's at the indices of the vectors contributing to this basis vector
+// can be changed from bitset to any other datastructure. make sure to cancel out indices that occur even number of times
+const int MAX_N = 501;
+struct VectorSpace{
+    bitset<MAX_N> basis[MAX_N];
+    bitset<MAX_N> index[MAX_N];
+    int s = 0;
+
+    // returns true in case of a new basis vector
+    bool insert(bitset<MAX_N> v, int idx)
+    {
+        bitset<MAX_N> cont;
+        for (int i = 0; i < MAX_N; ++i)
+        {
+            if (v[i])
+            {
+                if (basis[i].count())
+                {
+                    v ^= basis[i];
+                    cont ^= index[i];
+                }
+                else
+                {
+                    basis[i] = v;
+                    cont.set(idx);
+                    index[i] = cont;
+                    s++;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    // returns bitset with indices of contributing vectors. all zeros if impossible to construct
+    // careful if v is already all zeros. Special case meaning it can be constructed without any vector
+    bitset<MAX_N> can(bitset<MAX_N> v)
+    {
+        bitset<MAX_N> ret;
+        for (int i = 0; i < MAX_N; ++i)
+        {
+            if (v[i])
+            {
+                if (!basis[i].count())
+                {
+                    ret.reset();
+                    return ret;
+                }
+                v ^= basis[i];
+                ret ^= index[i];
+            }
+        }
+        return ret;
+    }
+};
