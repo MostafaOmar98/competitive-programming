d820ea80585f4ef31b193c33ad20628d1d79f0f6
MostafaOmar98 <mostafa.omar.m.s@gmail.com>
2021-02-02 08:40:27 +0200

    Polygon area

--- Algorithms/geometry/Polygon.cpp
+++ Algorithms/geometry/Polygon.cpp
@@ -9,3 +9,17 @@ ld regularPolygonArea(ld s, int n)
 {
     return (s * s * n)/(4 * tan(PI/n));
 }
+
+// returns double of a simple polygon area (can be non convex) int O(n)
+// returns double of the area so it's safe with integers (The only precision error is gonna be the 0.5 when you divide by 2)
+template <typename T>
+T doublePolygonArea(const vector<Point<T> > &v)
+{
+    T ret = 0;
+    for (int i = 1; i + 1 < sz(v); ++i)
+    {
+        Point<T> ab = v[i] - v[0], ac = v[i + 1] - v[0];
+        ret += ab.cross(ac);
+    }
+    return abs(ret);
+}
